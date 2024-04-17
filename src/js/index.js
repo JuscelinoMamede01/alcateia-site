6efffbdc59492eacfe84413c9df5d35c0691f167
JuscelinoMamede <juscemamede01@gmail.com>
2024-04-17 12:44:09 -0300

    construção do hero

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+// Verifique se a URL atual é a página inicial ou a página sobre
+const currentPath = window.location.pathname;
+const homeLink = document.getElementById("homeLink");
+const aboutLink = document.getElementById("aboutLink");
+const contactLink = document.getElementById("contactLink");
+
+if (currentPath === "/src/index.html") {
+  homeLink.classList.remove("text-white");
+  homeLink.classList.add("text-red-500");
+} else if (currentPath === "/src/pages/about.html") {
+  aboutLink.classList.remove("text-white");
+  aboutLink.classList.add("text-red-500");
+} else if (currentPath === "/src/pages/contact.html") {
+  contactLink.classList.remove("text-white");
+  contactLink.classList.add("text-red-500");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+
   theme: {
     container: {
       center: "true",
     },
-    extend: {},
+    extend: {
+      colors: {
+        color1: "#09090B",
+        color2: "#7690B6",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
